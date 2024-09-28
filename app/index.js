ef4b8e60ed33520c074849f2856287ce33748328
Amritpal <amritpal15k@gmail.com>
2024-09-28 17:21:30 +0530

    firebseAuth-using-reactNative

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View className="bg-red-200 pt-10 " >
+      <Text className="text-4xl text-center font-light" >Hello</Text>
+    </View>
+  )
+}
