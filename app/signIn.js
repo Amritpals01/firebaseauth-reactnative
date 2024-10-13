e1a6785c384c6de810f694d71ed7e8a0ecddeb59
Amritpals01 <amritpal15k@gmail.com>
2024-10-13 22:18:48 +0530

    signin

--- app/signIn.js
+++ app/signIn.js
@@ -1,5 +1,5 @@
 import { View, Text, Image, TextInput, Pressable, Alert } from "react-native";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -8,22 +8,22 @@ import { StatusBar } from "expo-status-bar";
 import { Octicons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
-
+import Loading from "../components/Loading";
 
 export default function SignIn() {
   const router = useRouter();
-  
+  const [loading, setLoading] = useState(false);
+
   const emailRef = useRef("");
   const passwordRef = useRef("");
 
-  const handelLogin = async() => {
+  const handelLogin = async () => {
     if (!emailRef.current || !passwordRef.current) {
-      Alert.alert('Sign In',"Please fill all the fields");
+      Alert.alert("Sign In", "Please fill all the fields");
       return;
     }
-      //Login process
-  }
-
+    //Login process
+  };
 
   return (
     <View className="flex-1">
@@ -43,7 +43,7 @@ export default function SignIn() {
         </View>
         <View className="pt-1">
           <Text
-            style={{ fontSize: hp(3),fontWeight: 'semibold', }}
+            style={{ fontSize: hp(3), fontWeight: "semibold" }}
             className="font-light tracking-widest text-center text-neutral-800 pb-8 shadow-md  "
           >
             SIGN IN
@@ -56,7 +56,7 @@ export default function SignIn() {
             >
               <Octicons name="mail" size={hp(2.7)} color="#504f54" />
               <TextInput
-                onChangeText={value=> emailRef.current=value}
+                onChangeText={(value) => (emailRef.current = value)}
                 style={{ fontSize: hp(2) }}
                 className="flex-1 text-neutral-800 font-semibold"
                 placeholder="Email Address"
@@ -70,7 +70,7 @@ export default function SignIn() {
               >
                 <Octicons name="lock" size={hp(2.7)} color="#504f54" />
                 <TextInput
-                onChangeText={value=> passwordRef.current=value}
+                  onChangeText={(value) => (passwordRef.current = value)}
                   style={{ fontSize: hp(2) }}
                   className="flex-1 text-neutral-800 font-semibold"
                   placeholder="Password"
@@ -87,27 +87,40 @@ export default function SignIn() {
               </Text>
             </View>
             {/* Submit button */}
+              {/* Loading animation */}
+            <View>
+              {loading ? (
+                <View className="flex-row justify-center">
+                  <Loading size={hp(10)} />
+                </View>
+              ) : (
+                <View className="justify-center items-center ">
+                  <TouchableOpacity
+                    onPress={handelLogin}
+                    style={{ height: hp(6.5), width: wp(50) }}
+                    className="bg-brown-500 rounded-xl justify-center items-center "
+                  >
+                    <Text
+                      style={{ fontSize: hp(2.3), fontWeight: "semibold" }}
+                      className="font-light tracking-wider w-29"
+                    >
+                      Submit
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+            </View>
+
             <View className="justify-center items-center ">
-              <TouchableOpacity onPress={handelLogin}
-                style={{ height: hp(6.5), width: wp(50) }}
-                className="bg-brown-500 rounded-xl justify-center items-center "
-              >
-                <Text
-                  style={{ fontSize: hp(2.3), fontWeight: 'semibold', }}
-                  className="font-light tracking-wider w-29"
-                >
-                  Submit
-                </Text>
-              </TouchableOpacity>
               {/* Sign up */}
-              <View className="flex-row justify-center pt-9 ">
+              <View className="flex-row justify-center pt-3 ">
                 <Text
                   style={{ fontSize: hp(1.8) }}
                   className="font-semibold text-neutral-800 "
                 >
                   Don't have an account?{" "}
                 </Text>
-                <Pressable onPress={()=> router.push('signUp')}>
+                <Pressable onPress={() => router.push("signUp")}>
                   <Text
                     style={{
                       fontSize: hp(1.8),
